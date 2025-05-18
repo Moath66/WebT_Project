5eced30d7c9f93b5d5998512877c09842dcc66d8
Moath66 <mohamed-a2@graduate.utm.my>
2025-05-18 15:18:05 +0800

    Initial commit of Personal Finance Tracker

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Personal Finance Dashboard</h2>
+    <SummaryCard />
+    <SpendingChart />
+    <SpendingTrend />
+    <TransactionList />
+  </div>
+</template>
+
+<script>
+import SummaryCard from "@/components/SummaryCard.vue";
+import SpendingChart from "@/components/SpendingChart.vue";
+import SpendingTrend from "@/components/SpendingTrend.vue";
+import TransactionList from "@/components/TransactionList.vue";
+
+export default {
+  components: { SummaryCard, SpendingChart, SpendingTrend, TransactionList },
+};
+</script>
---
+++ src/views/UpdateForm.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h2>Add New Transaction</h2>
+    <!-- Form will go here -->
+  </div>
+</template>
